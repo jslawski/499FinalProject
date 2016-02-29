0134bbd289f9d5c32d486a35634ec55cc2315a00
jslawski <jslawski@umich.edu>
2016-02-29 14:51:10 -0500

    Enchantment infrastructure added

---
+++ Assets/Scripts/EnchantmentKeys.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//These gem names are arbitrary.  We can change them to whatever we want
+//Gems used for enchanting a weapon:
+//Aquamarine:	Ice
+//Sunstone:		Fire
+//Fluorite:		Additional Crit
+//Onyx:			Additional Damage
+//Spinel:		Wounding	
+public enum Gems { Aquamarine, Sunstone, Fluorite, Onyx, Spinel, NumberOfTypes};
+
+public class EnchantmentKeys : MonoBehaviour {
+	//GLOBAL KEY VALUES FOR ENCHANTMENTS  
+	//Whenever an enchanted object is interacted with, (ex: weapon, armor, enemy, etc.) 
+	//access each enchantment in that object's dictionary, indexed by the following keys:
+
+	//ICE ENCHANTMENT
+	//Stone:  Aquamarine
+	//Weapon:		   Deals ice-based damage.  Contains a percent-chance (0-1) to slow an enemy's movements temporarily
+	//Armor/Enemies:   Resistance to ice-based damage.  Contains a percentage (0-1) of damage prevented from ice-based attacks.
+	//				   Prevents any slow-down effect from ice-enchanted weapons.
+	public static string ICE_KEY = "ice";
+
+	//FIRE ENCHANTMENT
+	//Stone:  Sunstone
+	//Weapon:			Deals fire-based damage.  Contains a percent-chance (0-1) to ignite an enemy, dealing continuous damage over a short period of time
+	//					(Potentially has the ability to ignite other enemies?)
+	//Armor/Enemies:	Resistance to fire-based damage.  Contains a percentage (0-1) of damage prevented from fire-based attacks.
+	//					Prevents any igniting effect from fire-enchanted weapons
+	public static string FIRE_KEY = "fire";
+
+	//ADDITIONAL CRIT ENCHANTMENT
+	//Stone:  Fluorite
+	//Weapon:			Contains a percentage (0-1) that is added to the weapon's critChance before damage is calculated every attack.
+	//Armor/Enemies:	Contains a percentage (0-1) that is added to the character's critChance before damage is calculated every attack.
+	//					NOTE: Do we want 2 different critChances? A base one for the character and another for the weapon?  They could both
+	//						  be rolled individually every attack so they're not additive.  We'll have to discuss.
+	public static string ADDITIONAL_CRIT_KEY = "additional_crit";
+
+	//ADDITIONAL DAMAGE ENCHANTMENT
+	//Stone:  Onyx
+	//Weapon:			Contains a percentage (0-1) that is applied to the weapon's damageMin and damageMax, then added to it.
+	//Armor/Enemies:	Contains a percentage (0-1) that is applied to the armor/enemy's damagePreventedMin and damagePreventedMax, then added to it.
+	public static string ADDITIONAL_DAMAGE_KEY = "additional_damage";
+
+	//WOUNDING ENCHANTMENT
+	//Stone:  Spinel
+	//Weapon:			Contains a percent-chance (0-1) to "wound" an enemy, causing them lose chunks of HP at regular intervals for a period of time.
+	//					(Potentially allow this ability to stack with each successful trigger?)
+	//Armor/Enemies:	Prevents any "wound" effect from wounding-enchanted weapons.  (Anything else?)
+	public static string WOUNDING_KEY = "wounding";
+}
--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -1,32 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public enum Rarity { Common, Uncommon, Rare };
 
 public class Weapon : MonoBehaviour {
 
-	protected int damageMin;			//Min damage a weapon can output without a critical hit
-	protected int damageMax;			//Max damage a weapon can output without a critical hit
-	protected float critChance;			//Percent chance from 0 - 1 of landing a critical hit
-	protected Rarity weaponRarity;		//Rarity of the weapon (determines its power)
-	protected int handsRequired;        //Number of hands needed to hold the weapon (1 or 2)
-	protected float attackCoolDown;		//Time in seconds that the player has to wait before being able to attack again
+	protected int damageMin;								//Min damage a weapon can output without a critical hit
+	protected int damageMax;								//Max damage a weapon can output without a critical hit
+	protected float critChance;								//Percent chance from 0 - 1 of landing a critical hit
+	protected Rarity weaponRarity;							//Rarity of the weapon (determines its power)
+	protected int handsRequired;							//Number of hands needed to hold the weapon (1 or 2)
+	protected float attackCoolDown;                         //Time in seconds that the player has to wait before being able to attack again
+
+	protected int numGemSlots;								//Number of slots a weapon has for enchantment gems
+	protected List<Gems> attachedGems;						//List of all gems attached to the weapon.  Used for calculating and storing values in weaponEnchantments
+	protected Dictionary<string, float> weaponEnchantments;	//Dict of all of the current enchantment abiltities on the weapon
 
 	// Use this for initialization
 	protected virtual void Start () {
 		//I would like to have this dynamically determined, sometimes by random chance, and other times depending on the chest it comes out of
 		//Having a "rare chest" that has a guaranteed rare item would be kind of cool.  For now, the rarity is preset in this function
 		weaponRarity = Rarity.Rare;
+
+		//For testing purposes, each weapon is given a random number of enchantment slots, with a random selection of gems.  
+		//I'd like to explore the possibility of varying the number of slots based on weapon type, weapon rarity, or other factors in the future.
+		//Gem slots will be populated only when the weapon is enchanted at a blacksmith.  For now, I'm just putting random ones in automatically
+		//for testing.
+		numGemSlots = Random.Range(1, 6);
+		attachedGems = new List<Gems>();
+		for (int i = 0; i < numGemSlots; i++) {
+			attachedGems.Add((Gems)Random.Range(0, (int)Gems.NumberOfTypes));
+		}
+
+		//I plan to populate this field with enchantment calculations next.  Right now it does nothing.
+		weaponEnchantments = new Dictionary<string, float>();
 	}
 
 	protected void PrintWeaponStats(string weaponType) {
 		Debug.Log("<b>Weapon Stats (" + weaponType + "):</b>\n<i>Rarity: </i>" + weaponRarity + "\n<i>Damage Min: </i>" + damageMin +
 				  "\n<i>Damage Max: </i>" + damageMax + "\n<i>Crit Chance: </i>" + critChance + "\n<i>Attack Cooldown: </i>" + attackCoolDown +
-				  "\n<i>Hands Required: </i>" + handsRequired);
+				  "\n<i>Hands Required: </i>" + handsRequired + "\n<i>Gems Equipped: </i>" + PrintGems());
     }
 
-	// Update is called once per frame
-	void Update () {
-	
+	string PrintGems() {
+		string returnValue = "";
+
+		foreach (Gems gem in attachedGems) {
+			returnValue += "\n     " + gem.ToString();
+		}
+		return returnValue;
 	}
 }
