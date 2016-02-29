bea047e9790db7dd0bc19b47ead24dca6ea3ff6a
jslawski <jslawski@umich.edu>
2016-02-29 01:09:14 -0500

    Initial commit without the meta files

---
+++ Assets/Scripts/Weapons/Dagger.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dagger : Weapon {
+
+	//Daggers are short-ranged weapons that only use 1 hand
+	//Of all of the weapons, they have the fastest attack speed.
+	//In terms of damage output, they are on the lower end, and their crit chance is about average
+
+	// Use this for initialization
+	protected override void Start () {
+		base.Start();
+
+		//These are arbitrarly picked numbers.  They can change as we see fit.
+		if (weaponRarity == Rarity.Common) {
+			damageMin = Random.Range(1, 2);
+			damageMax = Random.Range(4, 6);
+		}
+		else if (weaponRarity == Rarity.Uncommon) {
+			damageMin = Random.Range(4, 5);
+			damageMax = Random.Range(7, 10);
+		}
+		else if (weaponRarity == Rarity.Rare) {
+			damageMin = Random.Range(7, 9);
+			damageMax = Random.Range(13, 16);
+		}
+
+		handsRequired = 1;
+		attackCoolDown = 0.45f;
+
+		//Currently I was thinking we could have all weapons of the same type have the same crit chance
+		//I'm up for having this dependent on rarity as well
+		critChance = 0.05f;
+
+		PrintWeaponStats("Dagger");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Rarity { Common, Uncommon, Rare };
+
+public class Weapon : MonoBehaviour {
+
+	protected int damageMin;			//Min damage a weapon can output without a critical hit
+	protected int damageMax;			//Max damage a weapon can output without a critical hit
+	protected float critChance;			//Percent chance from 0 - 1 of landing a critical hit
+	protected Rarity weaponRarity;		//Rarity of the weapon (determines its power)
+	protected int handsRequired;        //Number of hands needed to hold the weapon (1 or 2)
+	protected float attackCoolDown;		//Time in seconds that the player has to wait before being able to attack again
+
+	// Use this for initialization
+	protected virtual void Start () {
+		//I would like to have this dynamically determined, sometimes by random chance, and other times depending on the chest it comes out of
+		//Having a "rare chest" that has a guaranteed rare item would be kind of cool.  For now, the rarity is preset in this function
+		weaponRarity = Rarity.Rare;
+	}
+
+	protected void PrintWeaponStats(string weaponType) {
+		Debug.Log("<b>Weapon Stats (" + weaponType + "):</b>\n<i>Rarity: </i>" + weaponRarity + "\n<i>Damage Min: </i>" + damageMin +
+				  "\n<i>Damage Max: </i>" + damageMax + "\n<i>Crit Chance: </i>" + critChance + "\n<i>Attack Cooldown: </i>" + attackCoolDown +
+				  "\n<i>Hands Required: </i>" + handsRequired);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
